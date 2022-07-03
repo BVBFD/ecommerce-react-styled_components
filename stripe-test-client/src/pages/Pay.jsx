0b3610dce82f9633f96529e4a981632fddf66e41
BVBFD <lsevina126@gmail.com>
2022-07-03 18:51:23 +0900

    code going to success page in payment client code

--- stripe-test-client/src/pages/Pay.jsx
+++ stripe-test-client/src/pages/Pay.jsx
@@ -1,10 +1,13 @@
 import axios from 'axios';
 import React, { useEffect, useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 import StripeCheckout from 'react-stripe-checkout';
 
 const Pay = () => {
   const [stripeToken, setStripeToken] = useState(null);
   const [price, setPrice] = useState(2000);
+
+  const navigate = useNavigate();
 
   const onToken = (token) => {
     setStripeToken(token);
@@ -21,6 +24,7 @@ const Pay = () => {
           }
         );
         console.log(res.data);
+        navigate('/success');
       } catch (error) {
         console.log(error);
       }
@@ -37,32 +41,36 @@ const Pay = () => {
         justifyContent: 'center',
       }}
     >
-      <StripeCheckout
-        label='Leo Shop'
-        name='Pay Now'
-        image='https://images.chosun.com/resizer/BzADectC5PmMDjNYKOg9mR6QDDA=/318x350/smart/cloudfront-ap-northeast-1.images.arcpublishing.com/chosun/OCDXXRBXIUEZJKXN4FLMGR4KOM.jpg'
-        billingAddress
-        shippingAddress
-        description={`your total is $${price / 100}`}
-        amount={price}
-        token={onToken}
-        stripeKey='pk_test_51LHLA7KDRarv62Gzb5RdKPWxdt9ZTLyMbsO82WK3R4nkbHpA1ZbliD6oxuCCRf0BPLd5xFSmFd5kk8FnFMhxpznO00xujpH1o1'
-      >
-        <button
-          style={{
-            border: 'none',
-            width: 120,
-            borderRadius: 5,
-            padding: '20px',
-            backgroundColor: 'black',
-            color: 'white',
-            fontWeight: '600',
-            cursor: 'pointer',
-          }}
+      {stripeToken ? (
+        <span>Processing. Please wait...</span>
+      ) : (
+        <StripeCheckout
+          label='Leo Shop'
+          name='Pay Now'
+          image='https://images.chosun.com/resizer/BzADectC5PmMDjNYKOg9mR6QDDA=/318x350/smart/cloudfront-ap-northeast-1.images.arcpublishing.com/chosun/OCDXXRBXIUEZJKXN4FLMGR4KOM.jpg'
+          billingAddress
+          shippingAddress
+          description={`your total is $${price / 100}`}
+          amount={price}
+          token={onToken}
+          stripeKey='pk_test_51LHLA7KDRarv62Gzb5RdKPWxdt9ZTLyMbsO82WK3R4nkbHpA1ZbliD6oxuCCRf0BPLd5xFSmFd5kk8FnFMhxpznO00xujpH1o1'
         >
-          Pay Now
-        </button>
-      </StripeCheckout>
+          <button
+            style={{
+              border: 'none',
+              width: 120,
+              borderRadius: 5,
+              padding: '20px',
+              backgroundColor: 'black',
+              color: 'white',
+              fontWeight: '600',
+              cursor: 'pointer',
+            }}
+          >
+            Pay Now
+          </button>
+        </StripeCheckout>
+      )}
     </div>
   );
 };
