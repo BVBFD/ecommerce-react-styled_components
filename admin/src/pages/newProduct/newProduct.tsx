5d9dd713d983e18f6da6773c7f223963e91f15a8
BVBFD <lsevina126@gmail.com>
2022-07-11 22:53:23 +0900

    handle multiple inputs in one state

--- admin/src/pages/newProduct/newProduct.tsx
+++ admin/src/pages/newProduct/newProduct.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { ReactEventHandler, useState } from 'react';
 import './newProduct.css';
 
 type SizeType = 'XS' | 'S' | 'M' | 'L' | 'XL' | '';
@@ -12,8 +12,32 @@ const NewProduct = () => {
   const [size, setSize] = useState<SizeType[]>([]);
   const [color, setColor] = useState<string[]>([]);
   const [price, setPrice] = useState<number>(0);
-  const [cat, setCat] = useState<CatType>();
+  const [cat, setCat] = useState<string[]>([]);
   const [stock, setStock] = useState<boolean>();
+
+  const handleChange = (
+    e: React.ChangeEvent<HTMLInputElement | HTMLSelectElement>
+  ) => {
+    setInputs((prev) => {
+      if (e.target.name === 'color') {
+        return { ...prev, [e.target.name]: e.target.value.split(',') };
+      }
+
+      if (e.target.name === 'size') {
+        return { ...prev, [e.target.name]: e.target.value.split(',') };
+      }
+
+      return { ...prev, [e.target.name]: e.target.value };
+    });
+  };
+
+  const handleCat = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setCat(e.target.value.split(','));
+  };
+
+  console.log(inputs);
+  console.log(file);
+  console.log(cat);
 
   return (
     <div className='newProduct'>
@@ -21,23 +45,48 @@ const NewProduct = () => {
       <form className='addProductForm'>
         <div className='addProductItem'>
           <label>Image</label>
-          <input type='file' id='file' />
+          <input
+            onChange={(e) => {
+              if (e.target.files == null) {
+                return;
+              }
+              return setFile(e.target.files[0]);
+            }}
+            type='file'
+            id='file'
+          />
         </div>
         <div className='addProductItem'>
           <label>Title</label>
-          <input name='title' type='text' placeholder='Apple Airpods' />
+          <input
+            onChange={handleChange}
+            name='title'
+            type='text'
+            placeholder='Apple Airpods'
+          />
         </div>
         <div className='addProductItem'>
           <label>Description</label>
-          <input name='desc' type='text' placeholder='description...' />
+          <input
+            onChange={handleChange}
+            name='desc'
+            type='text'
+            placeholder='description...'
+          />
         </div>
         <div className='addProductItem'>
           <label>Size</label>
-          <input name='size' type='text' placeholder='XS,S,M,L,XL...' />
+          <input
+            onChange={handleChange}
+            name='size'
+            type='text'
+            placeholder='XS,S,M,L,XL...'
+          />
         </div>
         <div className='addProductItem'>
           <label>Color</label>
           <input
+            onChange={handleChange}
             name='color'
             type='text'
             placeholder='yellow, red, gray, white...'
@@ -45,15 +94,24 @@ const NewProduct = () => {
         </div>
         <div className='addProductItem'>
           <label>Price</label>
-          <input name='price' type='number' placeholder='100' />
+          <input
+            onChange={handleChange}
+            name='price'
+            type='number'
+            placeholder='100'
+          />
         </div>
         <div className='addProductItem'>
           <label>Categories</label>
-          <input type='text' placeholder='women,man,shirt,dress...' />
+          <input
+            onChange={handleCat}
+            type='text'
+            placeholder='women,man,shirt,dress...'
+          />
         </div>
         <div className='addProductItem'>
           <label>Stock</label>
-          <select name='inStock'>
+          <select onChange={handleChange} name='inStock'>
             <option value='true'>Yes</option>
             <option value='false'>No</option>
           </select>
